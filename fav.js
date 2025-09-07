d7222607f05ba3f24bdca62339972a128658085c
chirag5999 <125373391+chirag5999@users.noreply.github.com>
2025-09-07 12:01:30 +0530

    Update fav.js

--- fav.js
+++ fav.js
@@ -1,140 +1,154 @@
-
-let favlist = JSON.parse(localStorage.getItem("favourites")) || [];
+let favList = JSON.parse(localStorage.getItem("favourites")) || [];
 let coins = [];
-let pageNo = 1;
-let itemsPerPage = 25;
+let page = 1;
+let itemsPerPage = 5;
 
-
-function renderFavCoins(coins,startIndex){
-    const tbody = document.querySelector("tbody");
-    tbody.innerHTML = ""; // Clear previous rows
-    coins.slice(startIndex,startIndex+25).forEach((coin, index) => {
-        row = document.createElement("tr");
-        // let isFav = favlist.includes(coin.id);
-        row.classList.add("coin-row");
-        row.setAttribute("data-id",coin.id);
-        row.innerHTML = `
-            <td>${(pageNo-1)*itemsPerPage + index + 1}</td>
-            <td><img src="${coin.image}" alt="${coin.name}" width="30"></td>
-            <td>${coin.name}</td>
-            <td>$${coin.current_price.toLocaleString()}</td>
-            <td>$${coin.total_volume.toLocaleString()}</td>
-            <td>$${coin.market_cap.toLocaleString()}</td>
-            <td><i class="fa-solid fa-trash" style = "color:#ec0909" data-id = "${coin.id}"></i></td>
-        `;
-        tbody.appendChild(row);
-    });
+function renderFavCoins(coins, startIndex) {
+  console.log(coins, startIndex, "data");
+  let tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  coins.slice(startIndex, startIndex + 5).forEach((coin, index) => {
+    let row = renderCoinsByRow(coin, index);
+    tbody.appendChild(row);
+  });
 }
 
+function renderCoinsByRow(coin, index) {
+  let row = document.createElement("tr");
+  row.classList.add("coin-row");
+  row.setAttribute("data-id", coin.id);
 
-async function fetchFavCoins(){
-    let res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${favlist.join(",")},tether&order=market_cap_desc&per_page=100&page=1&sparkline=false`);
-    coins = await res.json();
-    if(coins.length == 0){
-        console.error("No coin fetched");
-        title.style.display = "block";
-        table.style.display = "none";
-        pagination.style.display = "none";
-        return;
-    }
-    renderFavCoins(coins,0);
+  row.innerHTML = `
+    <td>${(page - 1) * itemsPerPage + index + 1}</td>
+    <td><img src="${coin.image}" alt="${
+    coin.name
+  }" width="16px" height="16px"/></td>
+    <td>${coin.name}</td>
+    <td>$${coin.current_price.toLocaleString()}</td>
+    <td>$${coin.total_volume.toLocaleString()}</td>
+    <td>$${coin.market_cap.toLocaleString()}</td>
+    <td><i class="fa-solid fa-trash" style="color: #ec0909;" data-id="${
+      coin.id
+    }"></i></td>
+    `;
+
+  return row;
 }
 
-function savefavs(){
-    localStorage.setItem("favourites",JSON.stringify(favlist));
+async function fetchFavCoins() {
+  let paginationContainer = document.querySelector(".pagination");
+  let tbody = document.querySelector("tbody");
+
+  if (favList.length == 0) {
+    paginationContainer.style.display = "none";
+    tbody.innerHTML = "";
+    let row = document.createElement("tr");
+    row.innerHTML = `<td colspan="7">No data to display</td>`;
+    tbody.appendChild(row);
+    return;
+  }
+  let res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${favList.join(
+      ","
+    )}&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+  );
+  if (!res.ok) throw new Error("API failed to fetch the data");
+  coins = await res.json();
+  renderFavCoins(coins, 0);
 }
 
-function removeCoin(coinId){
-    if(favlist.includes(coinId)){
-        favlist = favlist.filter((el) => el !== coinId);
-    }
-    savefavs();
-    fetchFavCoins();
+function saveFav() {
+  localStorage.setItem("favourites", JSON.stringify(favList));
 }
 
-document.addEventListener("click",(e) => {
-    if(e.target.classList.contains("fa-trash")){
-        removeCoin(e.target.dataset.id);
-    }
-
-    const row = e.target.closest(".coin-row");
-    if(row && !e.target.classList.contains("fa-star")){
-        const coinId = row.getAttribute("data-id");
-        window.location.href = `coin.html?id=${coinId}`
-    }
-})
+function removeCoin(coinId) {
+  if (favList.includes(coinId)) {
+    favList = favList.filter((el) => el !== coinId);
+  }
+  saveFav();
+  fetchFavCoins();
+}
 
 const handleSortPrice = (order) => {
-    coins.sort((a,b) => {
-        if(order === 'asc'){
-            return a.current_price - b.current_price;
-        }else{
-            return b.current_price - a.current_price;
-        }
-    })
-    renderFavCoins(coins);
-}
+  coins.sort((a, b) =>
+    order == "asc"
+      ? a.current_price - b.current_price
+      : b.current_price - a.current_price
+  );
+  renderFavCoins(coins);
+};
 const handleSortVolume = (order) => {
-    coins.sort((a,b) => {
-        if(order === 'asc'){    
-            return a.total_volume - b.total_volume;
-        }else{
-            return b.total_volume - a.total_volume;
-        }
-    })
-    renderFavCoins(coins);
-}   
+  coins.sort((a, b) =>
+    order == "asc"
+      ? a.total_volume - b.total_volume
+      : b.total_volume - a.total_volume
+  );
+  renderFavCoins(coins);
+};
 const handleSortMarketCap = (order) => {
-    coins.sort((a,b) => {
-        if(order === 'asc'){
-            return a.market_cap - b.market_cap;
-        }else{
-            return b.market_cap - a.market_cap;
-        }  
-    })
-    renderFavCoins(coins);
-}              
+  coins.sort((a, b) =>
+    order == "asc" ? a.market_cap - b.market_cap : b.market_cap - a.market_cap
+  );
+  renderFavCoins(coins);
+};
 
 const handleNext = () => {
-    pageNo++;
-    renderFavCoins(coins,(pageNo-1)*itemsPerPage);
-    updatePagination();
-}
+  page++;
+  renderFavCoins(coins, (page - 1) * itemsPerPage);
+  updatePagination();
+};
 
 const handlePrev = () => {
-    pageNo--;
-    renderFavCoins(coins,(pageNo-1)*itemsPerPage);
-    updatePagination();
+  page--;
+  renderFavCoins(coins, (page - 1) * itemsPerPage);
+  updatePagination();
+};
+
+function updatePagination() {
+  if (page == 1) document.querySelector(".prev-btn").disabled = true;
+  else document.querySelector(".prev-btn").disabled = false;
+
+  if (favList.length < page * itemsPerPage)
+    document.querySelector(".next-btn").disabled = true;
+  else document.querySelector(".next-btn").disabled = false;
 }
 
+document.addEventListener("click", (e) => {
+  console.log(e.target.classList.contains("fa-trash"), "boolean");
+  if (e.target.classList.contains("fa-trash")) {
+    removeCoin(e.target.dataset.id);
+  }
+});
 
-function updatePagination(){
-    if(pageNo == 1){
-        document.querySelector(".prev-btn").disabled = true;
-    }else{
-        document.querySelector(".prev-btn").disabled = false;
-    }
+document
+  .querySelector(".sort-asc-price")
+  .addEventListener("click", () => handleSortPrice("asc"));
+document
+  .querySelector(".sort-desc-price")
+  .addEventListener("click", () => handleSortPrice("desc"));
+document
+  .querySelector(".sort-asc-vol")
+  .addEventListener("click", () => handleSortVolume("asc"));
+document
+  .querySelector(".sort-desc-vol")
+  .addEventListener("click", () => handleSortVolume("desc"));
+document
+  .querySelector(".sort-asc-market")
+  .addEventListener("click", () => handleSortMarketCap("asc"));
+document
+  .querySelector(".sort-desc-market")
+  .addEventListener("click", () => handleSortMarketCap("desc"));
 
-    if(favlist.length < pageNo*itemsPerPage){
-        document.querySelector(".next-btn").disabled = true;
-    }else{
-        document.querySelector(".next-btn").disabled = false;
-    }
-}
+document.addEventListener("click", (e) => {
+  const row = e.target.closest(".coin-row");
+  if (row && !e.target.classList.contains("fa-star")) {
+    const coinId = row.getAttribute("data-id");
+    window.location.href = `coin.html?id=${coinId}`;
+  }
+});
 
-
-
-
-document.querySelector(".sort-asc-price").addEventListener('click', () => handleSortPrice('asc'));
-document.querySelector(".sort-desc-price").addEventListener('click',() => handleSortPrice('desc'));
-document.querySelector(".sort-asc-volume").addEventListener('click', () => handleSortVolume('asc'));
-document.querySelector(".sort-desc-volume").addEventListener('click',() => handleSortVolume('desc'));
-document.querySelector(".sort-asc-market").addEventListener('click', () => handleSortMarketCap('asc'));
-document.querySelector(".sort-desc-market").addEventListener('click',() => handleSortMarketCap('desc'));
-
-document.querySelector(".next-btn").addEventListener('click',handleNext);
-document.querySelector(".prev-btn").addEventListener('click',handlePrev);
-
+document.querySelector(".next-btn").addEventListener("click", handleNext);
+document.querySelector(".prev-btn").addEventListener("click", handlePrev);
 
 updatePagination();
 
